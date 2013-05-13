02cd8580a9f7770f8857880a4f172cbad1e10de6
bronhuston <bronhuston@gmail.com>
2013-05-13 15:30:53 -0700

    added non functional Melody Class

---
+++ src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/Melody.java
@@ -0,0 +1,35 @@
+package edu.ucsb.cs56.projects.music.basic_synthesis_demo;
+
+import java.util.ArrayList;
+
+/**
+    A Class that represents a Melody.
+    
+    
+    @author Bronwyn Perry-Huston
+    @version CS56 S13 for project 1 
+*/
+
+public class Melody extends ArrayList<Note>{
+
+
+	/**
+       no-arg constructor returns an empty melody
+       
+     */
+	public Melody(){
+		//call constructor for ArrayList<Note>, with capacity 1
+		super(1);
+	}
+
+	public void play(EnvelopedSound e, Melody m)
+	{
+
+	}
+	
+
+
+
+
+
+}

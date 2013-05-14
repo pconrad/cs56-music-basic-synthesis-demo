a1fd3180b0b7472198aab904dc2f0edc248332da
Chris Atanasian <catanasian@gmail.com>
2013-05-14 13:03:27 -0700

    improved readability of main and added spacing between methods

--- src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/ADSREnvelopedContinuousSound.java
+++ src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/ADSREnvelopedContinuousSound.java
@@ -54,36 +54,45 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
     public double getAmplitude() {
 	return amplitude;
     }
+    
     public void setAmplitude(double amp) {
 	this.amplitude = amp;
     }
+    
     public double getAttackTime() {
 	return attackTime;
     }
+    
     public void setAttackTime(double t) {
 	this.totalTime -= this.attackTime;
 	this.attackTime = t;
 	this.totalTime += this.attackTime;
     }
+    
     public double getDecayTime() {
 	return decayTime;
     }
+    
     public void setDecayTime(double t) {
 	this.totalTime -= this.decayTime;
 	this.decayTime = t;
 	this.totalTime += this.decayTime;
     }
+    
     public double getReleaseTime() {
 	return releaseTime;
     }
+    
     public void setReleaseTime(double t) {
 	this.totalTime -= this.releaseTime;
 	this.releaseTime = t;
 	this.totalTime += this.releaseTime;
     }
+    
     public double getSustainTime() {
 	return sustainTime;
     }
+    
     public void setSustainTime(double t) {
 	this.totalTime -= this.sustainTime;
 	this.sustainTime = t;
@@ -93,6 +102,7 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
     public double getSustainAmplitude() {
 	return sustainAmplitude;
     }
+    
     public void setSustainAmplitude(double amp) {
 	this.sustainAmplitude = amp;
     }
@@ -135,14 +145,17 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
      * executes the program
      */
     public static void main(String[] args) {
-	if(args.length!=7){
-	    System.out.println("frequency, amp, attack, decay, sustain amp, sustain time, release");
+	if (args.length != 7) {
+	    System.out.print("frequency, amp, attack, decay, sustain amp,");
+	    System.out.println("sustain time, release");
 	    System.exit(1);
 	}
 
-	AudioFormat f= new AudioFormat(44100,8,1,true,true);
-	try{
-	    //Create a new audioLine which goes to the system, the audio format specifys all the features of the line.
+	AudioFormat f = new AudioFormat(44100,8,1,true,true);
+	try {
+	    /* Create a new audioLine which goes to the system, 
+	       the audio format specifys all the features of the line.
+	    */
 	    SourceDataLine d = AudioSystem.getSourceDataLine(f);
 	    ADSREnvelopedContinuousSound env =
 		new ADSREnvelopedContinuousSound(Double.parseDouble(args[0]),
@@ -158,7 +171,7 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
 	    d.start();
 	    env.load();
 	    d.drain();
-	}catch (Exception ex){
+	} catch (Exception ex) {
 	    ex.printStackTrace();
 	}
     }

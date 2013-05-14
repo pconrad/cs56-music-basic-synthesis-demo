bd6da10637832962c0b023aad45541960b5b6e52
Chris Atanasian <catanasian@gmail.com>
2013-05-14 15:34:48 -0700

    fixed some indents and javadoc comment in AudioDrain.java

--- src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/old/AudioDrain.java
+++ src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/old/AudioDrain.java
@@ -4,25 +4,24 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.SourceDataLine;
 
-/*
- creates a new thread that consistently drains the audioData [] byte array,
- causing the note to play
+/**
+   creates a new thread that consistently drains the audioData [] byte array,
+   causing the note to play
 */	
-
 public class AudioDrain implements Runnable {
 
-	SourceDataLine runMe;
+    SourceDataLine runMe;
 
-	public AudioDrain(SourceDataLine inputData) {
-		runMe=inputData;
+    public AudioDrain(SourceDataLine inputData) {
+	runMe = inputData;
+    }
+
+    public void run() {
+	try {
+	    runMe.drain();
+	} catch(Exception ex) {
+	    ex.printStackTrace();
 	}
-		
-	public void run() {
-		try{
-		    runMe.drain();
-		} catch(Exception ex) {
-		    ex.printStackTrace();
-		}
-	}
+    }
 }
 	

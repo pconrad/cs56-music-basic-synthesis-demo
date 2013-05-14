d4cbc3d7d3caa3ced50cf2f7d8213aadc8760be4
Chris Atanasian <catanasian@gmail.com>
2013-05-14 15:03:43 -0700

    added javadoc comments and improved readability even further

--- src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/ADSREnvelopedContinuousSound.java
+++ src/edu/ucsb/cs56/projects/music/basic_synthesis_demo/ADSREnvelopedContinuousSound.java
@@ -50,63 +50,101 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
 	this.releaseTime = releaseTime;
 	generateEnvelope();
     }
-    
+    /**
+       @return the amplitude
+    */
     public double getAmplitude() {
 	return amplitude;
     }
     
+    /**
+       sets the amplitude
+    */
     public void setAmplitude(double amp) {
 	this.amplitude = amp;
     }
     
+    /**
+       @return the attackTime
+    */
     public double getAttackTime() {
 	return attackTime;
     }
     
+    /**
+       sets the attackTime and changes the totalTime accordingly
+    */
     public void setAttackTime(double t) {
 	this.totalTime -= this.attackTime;
 	this.attackTime = t;
 	this.totalTime += this.attackTime;
     }
     
+    /**
+       @return the decayTime
+    */
     public double getDecayTime() {
 	return decayTime;
     }
     
+    /**
+       sets the decayTime and changes totalTime accordingly
+    */
     public void setDecayTime(double t) {
 	this.totalTime -= this.decayTime;
 	this.decayTime = t;
 	this.totalTime += this.decayTime;
     }
     
+    /**
+       @return the releaseTime
+    */
     public double getReleaseTime() {
 	return releaseTime;
     }
     
+    /**
+       sets the releaseTime and changes totalTime accordingly
+    */
     public void setReleaseTime(double t) {
 	this.totalTime -= this.releaseTime;
 	this.releaseTime = t;
 	this.totalTime += this.releaseTime;
     }
     
+    /**
+       @return the sustainTime
+    */
     public double getSustainTime() {
 	return sustainTime;
     }
     
+    /**
+       sets the sustainTime and canges totalTime accordingly
+    */
     public void setSustainTime(double t) {
 	this.totalTime -= this.sustainTime;
 	this.sustainTime = t;
 	this.totalTime += this.sustainTime;
     }
  
+    /**
+       @return the sustainAmplitude
+    */
     public double getSustainAmplitude() {
 	return sustainAmplitude;
     }
     
+    /**
+       sets the sustainAmplitude and canges totalTime accordingly
+    */
     public void setSustainAmplitude(double amp) {
 	this.sustainAmplitude = amp;
     }
     
+    /**
+       @return the private instance variables in a String
+    */
     public String toString() {
         return super.toString() + " Amplitude: " + amplitude + 
 	    " Sustain Amplitude: " + sustainAmplitude +
@@ -118,23 +156,20 @@ public class ADSREnvelopedContinuousSound extends EnvelopedSound {
 
     protected double generateAmplitude(double time, double amplitude) {
 	double amp = amplitude;
-	if (time<this.attackTime) {
+	if (time < this.attackTime) {
 	    //the rate of growth of amplitude per sample
 	    double deltaAmp = this.amplitude/(getSampleRate()*this.attackTime);
 	    return amplitude + deltaAmp;
-	}
-	else if (time < this.attackTime + decayTime) {
+	} else if (time < this.attackTime + decayTime) {
 	    double a = amplitude - this.sustainAmplitude;
 	    //when amplitude =.0001*A[max value of 1] 127*A=0;
 	    double k = Math.log(.001) / (this.decayTime*getSampleRate());
 	    if(DEBUG && time < .26)
 		System.out.println(k+" "+a+" "+decayTime+" "+Math.log(.001));
 	    return amplitude + k*a;
-	}
-	else if (time < this.attackTime + this.decayTime + this.sustainTime) {
+	} else if (time < this.attackTime + this.decayTime + this.sustainTime) {
 	    return amplitude;
-	}
-	else {
+	} else {
 	    //when amplitude =.0001*A[max value of 1] 127*A=0;
 	    double k = Math.log(.001) / (this.releaseTime * getSampleRate());
 	    return amplitude + k*amplitude;
